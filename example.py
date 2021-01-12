bde057fb98a31f097af1dd223463d15c83dfe843
Wlad Meixner <9556979+gosticks@users.noreply.github.com>
2021-01-12 12:32:11 +0100

    add visualizer example

--- example.py
+++ example.py
@@ -1,34 +1,53 @@
 import yaml
+import torch
 
-from loader import *
+from model import *
+from renderer import *
+from dataset import *
 
 ascii_logo = """\
   /$$$$$$  /$$      /$$ /$$$$$$$  /$$   /$$     /$$
  /$$__  $$| $$$    /$$$| $$__  $$| $$  |  $$   /$$/
-| $$  \__/| $$$$  /$$$$| $$  \ $$| $$   \  $$ /$$/ 
-|  $$$$$$ | $$ $$/$$ $$| $$$$$$$/| $$    \  $$$$/  
- \____  $$| $$  $$$| $$| $$____/ | $$     \  $$/   
- /$$  \ $$| $$\  $ | $$| $$      | $$      | $$    
-|  $$$$$$/| $$ \/  | $$| $$      | $$$$$$$$| $$    
- \______/ |__/     |__/|__/      |________/|__/    
-                                                   
+| $$  \__/| $$$$  /$$$$| $$  \ $$| $$   \  $$ /$$/
+|  $$$$$$ | $$ $$/$$ $$| $$$$$$$/| $$    \  $$$$/
+ \____  $$| $$  $$$| $$| $$____/ | $$     \  $$/
+ /$$  \ $$| $$\  $ | $$| $$      | $$      | $$
+|  $$$$$$/| $$ \/  | $$| $$      | $$$$$$$$| $$
+ \______/ |__/     |__/|__/      |________/|__/
+
 """
+
 
 def load_config():
     with open('./config.yaml') as file:
-    # The FullLoader parameter handles the conversion from YAML
-    # scalar values to Python the dictionary format
+        # The FullLoader parameter handles the conversion from YAML
+        # scalar values to Python the dictionary format
         config = yaml.load(file, Loader=yaml.FullLoader)
 
     return config
+
 
 def main():
     print(ascii_logo)
     conf = load_config()
     print("config loaded")
-    l = DataLoader(conf['inputPath'], conf['modelPath'])
-    # l.show_cur_item()
-    l.create_model()
-    
+
+    dataset = SMPLyDataset()
+
+    l = SMPLyModel(conf['modelPath'])
+    r = SMPLyRenderer()
+    m = l.create_model()
+
+    keypoints = dataset[0]
+    print(keypoints)
+    betas = torch.randn([1, 10], dtype=torch.float32)
+
+    # torch.randn((1, 69), dtype=torch.float32) * 0.7
+    body_pose = torch.zeros((1, 69))
+    body_pose = torch.from_numpy(keypoints.reshape((1, 69)))
+    print(body_pose)
+    r.display_model(m, betas=betas, keypoints=keypoints, body_pose=body_pose)
+
+
 if __name__ == '__main__':
     main()
